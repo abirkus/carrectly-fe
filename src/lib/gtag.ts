fb8570929b6eb256fd78db2a82ca8939a67ff4e1
Andre Birkus <andre.birkus@deep6.ai>
2022-03-22 20:53:10 -0500

    adding fb tracking

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,3 +1,5 @@
+// nextjs example https://github.com/vercel/next.js/tree/canary/examples/with-google-analytics
+
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
