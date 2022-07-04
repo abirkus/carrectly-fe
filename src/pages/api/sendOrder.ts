3050885104e97d67938090af2d1722775d0ef9a6
Valeriia Braterska <val.braterskaya@gmail.com>
2022-07-04 13:11:04 +0300

    Revert "update name for endpoint"

--- src/pages/api/sendOrder.ts
+++ src/pages/api/sendOrder.ts
@@ -3,10 +3,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const response = await axios.post(`${process.env.CARRECTLY_ADMIN_URL}/api/booking`, {
+    const response = await axios.post(`${process.env.CARRECTLY_ADMIN_URL}/api/newBooking`, {
       param: req.body,
     });
-    await axios.post('https://carrectlyautocare.com/api/booking', {
+    await axios.post('https://carrectlyautocare.com/api/newBooking', {
       param: req.body,
     });
     res.status(200).json(response.statusText);
